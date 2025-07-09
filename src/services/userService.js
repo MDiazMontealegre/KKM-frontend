69bcf9391f35a8151ddedfb6769d2b476c4f52fa
MateoDiaz <matdimo01@gmail.com>
2025-07-09 04:08:07 -0500

    userservice

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,5 @@
 //1. Creacion de servicios
-const API_BASE = "https://kkm-backend.onrender.com";
+const API_BASE = "https://kkm-backend.onrender.com/docs#/User";
 
 export const getUsers = async() => {
     const response = await fetch(`${API_BASE}/user/get-users`)
